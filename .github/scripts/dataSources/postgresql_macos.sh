c9dcfbd3e607c22401fa230fb329de5ff3472f2c
SolomonAnn <thss15_anyz@163.com>
2023-10-19 22:35:50 +0800

    Fix

--- .github/scripts/dataSources/postgresql_macos.sh
+++ .github/scripts/dataSources/postgresql_macos.sh
@@ -42,9 +42,9 @@ do
 
   sh -c "sudo chmod -R 777 /var/lib/postgresql-$port/15/main"
 
-  sh -c "cd pgsql-$port/bin; echo \"postgres\" | sudo -u postgres -S sh -c './initdb -D /var/lib/postgresql-$port/15/main --auth trust --no-instructions'"
+  sh -c "cd pgsql-$port/bin; sudo -u postgres -S <<< \"postgres\" bash -c './initdb -D /var/lib/postgresql-$port/15/main --auth trust --no-instructions'"
 
-  sh -c "cd pgsql-$port/bin; echo \"postgres\" | sudo -u postgres -S sh -c './pg_ctl -D /var/lib/postgresql-$port/15/main -o \"-F -p $port\" start'"
+  sh -c "cd pgsql-$port/bin; sudo -u postgres -S <<< \"postgres\" bash -c './pg_ctl -D /var/lib/postgresql-$port/15/main -o \"-F -p $port\" start'"
 
-  sh -c "cd pgsql-$port/bin; echo \"postgres\" | sudo -u postgres -S sh -c './psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"'"
+  sh -c "cd pgsql-$port/bin; sudo -u postgres -S <<< \"postgres\" bash -c './psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"'"
 done
