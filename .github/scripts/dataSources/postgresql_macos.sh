4919c8f5e22042f180dd1ed99ea8849eec89379d
SolomonAnn <thss15_anyz@163.com>
2023-10-20 10:38:06 +0800

    Fix

--- .github/scripts/dataSources/postgresql_macos.sh
+++ .github/scripts/dataSources/postgresql_macos.sh
@@ -10,29 +10,29 @@ sh -c "wget --quiet https://get.enterprisedb.com/postgresql/postgresql-15.2-1-os
 
 sh -c "sudo unzip -q postgresql-15.2-1-osx-binaries.zip"
 
-sh -c "sudo dscl . -create /Users/postgres"
+sh -c "sudo dscl . -create /Users/postgres UniqueID 666"
+
+sh -c "sudo dscl . -create /Groups/postgres PrimaryGroupID 777"
+
+sh -c "sudo dscl . -create /Users/postgres PrimaryGroupID 777"
 
 sh -c "sudo dscl . -create /Users/postgres UserShell /bin/bash"
 
 sh -c "sudo dscl . -create /Users/postgres RealName \"PostgreSQL\""
 
-sh -c "sudo dscl . -create /Users/postgres UniqueID 666"
+sh -c "sudo dscl . create /Groups/postgres passwd '*'"
 
-sh -c "sudo dscl . -create /Users/postgres PrimaryGroupID 20"
+sh -c "sudo mkdir /Users/postgres"
+
+sh -c "chown -R postgres:postgres /Users/postgres"
 
 sh -c "sudo dscl . -create /Users/postgres NFSHomeDirectory /Users/postgres"
-
-sh -c "sudo dscl . -passwd /Users/postgres postgres"
-
-sh -c "sudo dscl . -append /Groups/admin GroupMembership postgres"
 
 sh -c "sudo mkdir -p /var/lib/postgresql/15/main"
 
 sh -c "sudo chown -R postgres /var/lib/postgresql/15/main"
 
 sh -c "sudo chmod -R 777 /var/lib/postgresql/15/main"
-
-password="postgres"
 
 for port in "$@"
 do
@@ -44,11 +44,11 @@ do
 
   sh -c "sudo chmod -R 777 /var/lib/postgresql-$port/15/main"
 
-  echo "cd pgsql-$port/bin; sudo -S -u postgres bash -c './initdb -D /var/lib/postgresql-$port/15/main --auth trust --no-instructions' <<< \"$password\""
+  echo "cd pgsql-$port/bin; sudo -u postgres /bin/bash -c './initdb -D /var/lib/postgresql-$port/15/main --auth trust --no-instructions'"
 
-  sh -c "cd pgsql-$port/bin; sudo -S -u postgres bash -c './initdb -D /var/lib/postgresql-$port/15/main --auth trust --no-instructions' <<< \"$password\""
+  sh -c "cd pgsql-$port/bin; sudo -u postgres /bin/bash -c './initdb -D /var/lib/postgresql-$port/15/main --auth trust --no-instructions'"
 
-  sh -c "cd pgsql-$port/bin; sudo -S -u postgres bash -c './pg_ctl -D /var/lib/postgresql-$port/15/main -o \"-F -p $port\" start' <<< \"$password\""
+  sh -c "cd pgsql-$port/bin; sudo -u postgres /bin/bash -c './pg_ctl -D /var/lib/postgresql-$port/15/main -o \"-F -p $port\" start'"
 
-  sh -c "cd pgsql-$port/bin; sudo -S -u postgres bash -c './psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"' <<< \"$password\""
+  sh -c "cd pgsql-$port/bin; sudo -u postgres /bin/bash -c './psql -c \"ALTER USER postgres WITH PASSWORD '\''postgres'\'';\"'"
 done
